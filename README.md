706bebdbda6af34c0d7255b27bfbd5b4e524c21b
Samsul Hadi <samsulhadi@Samsuls-MacBook-Air-2.local>
2021-03-21 10:10:21 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# GSP766
+
+Managing a GKE Multi-tenant Cluster with Namespaces
+
+https://www.qwiklabs.com/focuses/14861?parent=catalog
+
+# CloudShell
+> git clone https://github.com/gablooge/GSP766.git
+
+> cd GSP766
+
+> chmod +x cloudshell.sh
+
+> ./cloudshell.sh 
+
---
+++ cloudshell.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+
+# Task 1
+gsutil -m cp -r gs://spls/gsp766/gke-qwiklab ~
+
+cd ~/gke-qwiklab
+
+gcloud config set compute/zone us-central1-a && gcloud container clusters get-credentials multi-tenant-cluster
+
+kubectl get namespace
+
+kubectl api-resources --namespaced=true
+
+kubectl get services --namespace=kube-system
+
+kubectl create namespace team-a && \
+kubectl create namespace team-b
+
+kubectl run app-server --image=centos --namespace=team-a -- sleep infinity && \
+kubectl run app-server --image=centos --namespace=team-b -- sleep infinity
+
+# Task 2
+
+kubectl describe pod app-server --namespace=team-a
+kubectl config set-context --current --namespace=team-a
+kubectl describe pod app-server
+
+export GOOGLE_CLOUD_PROJECT=$(gcloud info --format='value(config.project)')
+
+gcloud projects add-iam-policy-binding ${GOOGLE_CLOUD_PROJECT} \
+--member=serviceAccount:team-a-dev@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com  \
+--role=roles/container.clusterViewer
+
+kubectl create role pod-reader \
+--resource=pods --verb=watch --verb=get --verb=list
+
+kubectl create -f developer-role.yaml
+
+kubectl create rolebinding team-a-developers \
+--role=developer --user=team-a-dev@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
+
+gcloud iam service-accounts keys create key.json --iam-account team-a-dev@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
+
+# Task 3
+gcloud auth activate-service-account  --key-file=key.json
+gcloud container clusters get-credentials multi-tenant-cluster --zone us-central1-a --project ${GOOGLE_CLOUD_PROJECT}
+gcloud container clusters get-credentials multi-tenant-cluster --zone us-central1-a --project ${GOOGLE_CLOUD_PROJECT}
+
+kubectl create quota test-quota \
+--hard=count/pods=2,count/services.loadbalancers=1 --namespace=team-a
+
+kubectl run app-server-2 --image=centos --namespace=team-a -- sleep infinity
+
+kubectl run app-server-3 --image=centos --namespace=team-a -- sleep infinity
+
+export KUBE_EDITOR="nano"
+kubectl edit quota test-quota --namespace=team-a
+
+kubectl describe quota test-quota --namespace=team-a
+
+kubectl create -f cpu-mem-quota.yaml
+
+kubectl create -f cpu-mem-demo-pod.yaml --namespace=team-a
+
+kubectl describe quota cpu-mem-quota --namespace=team-a
+
+# Task 4
+
+gcloud container clusters \
+update multi-tenant-cluster --zone us-central1-a \
+--resource-usage-bigquery-dataset cluster_dataset
+export GCP_BILLING_EXPORT_TABLE_FULL_PATH=${GOOGLE_CLOUD_PROJECT}.billing_dataset.gcp_billing_export_v1_xxxx
+export USAGE_METERING_DATASET_ID=cluster_dataset
+export COST_BREAKDOWN_TABLE_ID=usage_metering_cost_breakdown
+
+export USAGE_METERING_QUERY_TEMPLATE=~/gke-qwiklab/usage_metering_query_template.sql
+export USAGE_METERING_QUERY=cost_breakdown_query.sql
+export USAGE_METERING_START_DATE=2020-10-26
+
+sed \
+-e "s/\${fullGCPBillingExportTableID}/$GCP_BILLING_EXPORT_TABLE_FULL_PATH/" \
+-e "s/\${projectID}/$GOOGLE_CLOUD_PROJECT/" \
+-e "s/\${datasetID}/$USAGE_METERING_DATASET_ID/" \
+-e "s/\${startDate}/$USAGE_METERING_START_DATE/" \
+"$USAGE_METERING_QUERY_TEMPLATE" \
+> "$USAGE_METERING_QUERY"
+
+bq query \
+--project_id=$GOOGLE_CLOUD_PROJECT \
+--use_legacy_sql=false \
+--destination_table=$USAGE_METERING_DATASET_ID.$COST_BREAKDOWN_TABLE_ID \
+--schedule='every 24 hours' \
+--display_name="GKE Usage Metering Cost Breakdown Scheduled Query" \
+--replace=true \
+"$(cat $USAGE_METERING_QUERY)"
+
